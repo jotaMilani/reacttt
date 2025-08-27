5b49c1aff6c929dde4137a031346c0898a39a3e5
milani <fandangoslord2@gmail.com>
2025-08-27 15:23:57 -0300

    Adicionar funcionalidade de persistência de tarefas com AsyncStorage e melhorias na interface

--- 03-ListaDeTarefas/App.js
+++ 03-ListaDeTarefas/App.js
@@ -10,71 +10,134 @@ import {
   View,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [tasks, setTasks] = useState([]); //Estado para armazenar a lista de tarefas
-  const [newTask, setNewTask] = useState(""); //Estado para o texto da nova tarefa
+  //variavel de armazenamento das tarefas
+  const [task, setTask] = useState([]);
+  //estado para o texto da tarefa
+  const [newtask, setNewtask] = useState("");
 
-  const addTask = () => {
-    if (newTask.trim().length > 0) {
-      //Garante que a tarefa não seja vazia
-      setTasks((prevTasks) => [
-        ...prevTasks,
-        { id: Date.now().toString(), text: newTask.trim(), completed: false }, //Cria uma nova tarefa com id unico
-      ]);
-      setNewTask(""); // Limpar o campo de input
-      Keyboard.dismiss(); // Fecha o teclado do usuário
-    } else {
-      Alert.alert("Atenção", "Por favor, digite uma tarefa.");
+useEffect(() => {
+  const loadTasks = async () => {
+    try{
+      const savedTasks = await AsyncStorage.getItem("tasks");
+      savedTasks && setStatusBarNetworkActivityIndicatorVisible(JSON.parse(savedTasks));
+    } catch (error) {
+      console.error("Erro ao carregar tarefas:", error)
+    }
+  }
+  loadTasks();
+} )
+
+
+useEffect(()=>{
+  const saveTasks = async () => {
+    try{
+      await AsyncStorage.setItem("task", JSON.stringify(task))
+    } catch (error) {
+      console.error("Erro ao salvar tarefas", error);
     }
   };
 
+saveTasks();
+
+},[task])
+
+  const addtask = () => {
+    if (newtask.trim().length > 0) {
+      setTask((prevtask) => [
+        ...prevtask,
+        {
+          id: Date.now().toString(),
+          text: newtask.trim(),
+          completed: false,
+        },
+      ]);
+      setNewtask("");
+      Keyboard.dismiss();
+    } else {
+      Alert.alert("Atenção", "Por favor informe uma tarefa");
+    }
+  };
+
+  const toggleCompleteted = (id) => {
+    setTask((prevTask) =>
+      prevTask.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    Alert.alert(
+      "confirmar exclusão",
+      "Tem certeza que deseja excluir essa tarefa?",
+      [
+        { text: "cancelar", style: "cancel" },
+        {
+          text: "Excluir",
+          style: "destructive",
+          onPress: () =>
+            setTask((prev) => prev.filter((task) => task.id !== id)),
+        },
+      ]
+    );
+  };
+
+  const renderList = ({ item }) => (
+    <View style={styles.taskItem} key={item.id}>
+      <TouchableOpacity
+        style={styles.taskTextContainer}
+        onPress={() => toggleCompleteted(item.id)}
+      >
+        <Text
+          style={[styles.taskText, item.completed && styles.completedTaskItem]}
+        >
+          {item.text}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => deleteTask(item.id)}>
+        <Text style={styles.taskText}>🗑️</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      {/* cabeçalho */}
       <View style={styles.topBar}>
-        <Text style={styles.topBarTitle}>Minhas Tarefas</Text>
+        <Text style={styles.topBarTittle}>Minhas Tarefas</Text>
         <TouchableOpacity>
           <Text>🌛</Text>
         </TouchableOpacity>
       </View>
 
-      {/* Local onde o usuário insere as tarefas */}
+      {/* local onde o usuario insere as tarefas */}
       <View style={styles.card}>
         <TextInput
           style={styles.input}
           placeholder="Adicionar nova tarefa..."
-          value={newTask}
-          onChangeText={setNewTask}
-          onSubmitEditing={addTask} //Adiciona a tarefa ao pressionar Enter no teclado
+          value={newtask}
+          onChangeText={setNewtask} //onChangeText especifica somente o texto
+          onSubmitEditing={addtask} //executa a função quando o usuario clicar em "ok no Keyboard(teclado)"
         />
-        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+        <TouchableOpacity style={styles.addButton} onPress={addtask}>
           <Text style={styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
       </View>
-
-      {/* Lista de tarefas do usuário */}
+      {/* lista de tarefas */}
       <FlatList
-        style={styles.flatList}
-        data={tasks}
+        style={[styles.flatList]}
+        data={task}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View key={item.id} style={styles.taskItem}>
-            <Text>{item.text}</Text>
-            <TouchableOpacity>
-              <Text>🗑️</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        renderItem={renderList}
         ListEmptyComponent={() => (
           <Text style={styles.emptyListText}>
-            Nenhuma tarefa adicionada ainda.
+            Nenhuma tarefa adicionando ainda
           </Text>
         )}
         contentContainerStyle={styles.flatListContent}
       />
-
       <StatusBar style="auto" />
     </View>
   );
@@ -85,7 +148,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#e0f7fa",
   },
-
   topBar: {
     backgroundColor: "#fff",
     flexDirection: "row",
@@ -95,15 +157,13 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingBottom: 15,
     borderBottomWidth: 1,
-    borderBottomColor: "rgba(0,0,0,0.1)",
+    borderBottomColor: "rgba(0,0,0, 0.1)",
   },
-
-  topBarTitle: {
+  topBarTittle: {
     color: "#051650",
     fontSize: 24,
     fontWeight: "bold",
   },
-
   card: {
     backgroundColor: "#fff",
     color: "#000",
@@ -116,7 +176,6 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 10,
   },
-
   input: {
     backgroundColor: "#fcfcfc",
     color: "#333",
@@ -127,66 +186,58 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 10,
   },
-
   addButton: {
     backgroundColor: "#123499",
     paddingVertical: 12,
     borderRadius: 10,
     alignItems: "center",
   },
-
   buttonText: {
     color: "#fff",
+    fontSize: 18,
     fontWeight: "bold",
   },
-
   flatListContent: {
-    paddingBottom: 10,
+    paddingBottom: 10, //espaçamento no final da lista
   },
-
   taskItem: {
     backgroundColor: "#fff",
     color: "#333",
-    borderColor: "rgba(0,0,0,0.1",
+    borderColor: "rgba(0,0,0, 0.1)",
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
+    borderRadius: 10,
     padding: 15,
     marginVertical: 10,
     marginHorizontal: 15,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
-    elevation: 5,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 10,
     borderWidth: 1,
   },
-
   taskTextContainer: {
     flex: 1,
     marginRight: 10,
   },
-
   taskText: {
     color: "#333",
     fontSize: 18,
     flexWrap: "wrap",
   },
-
   completedTaskItem: {
     textDecorationLine: "line-through",
     opacity: 0.6,
   },
-
   deleteButton: {
     padding: 8,
     borderRadius: 5,
   },
-
   deleteButtonText: {
     fontSize: 22,
     fontWeight: "bold",
   },
-
   emptyListText: {
     color: "#9e9e9e",
     textAlign: "center",
